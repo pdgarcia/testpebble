09ebc9d3c76ba8073db5bab8f74370a07c7bf4e8
Pablo Garcia <pdgarcia@users.noreply.github.com>
2015-09-08 22:52:22 +0200

    Added Date

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_weather_layer;
+static TextLayer *s_time_layer, *s_date_layer;
 
 static GFont s_time_font;
 static GFont s_weather_font;
+static GFont s_time_font, s_date_font;
 
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
@@ -20,7 +22,8 @@ static void update_time() {
 
   // Create a long-lived buffer
   static char buffer[] = "00:00";
-
+  static char date_buffer[16];
+  
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     //Use 2h hour format
@@ -32,6 +35,12 @@ static void update_time() {
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  
+  // Copy date into buffer from tm structure
+  strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
+  // Show the date
+  text_layer_set_text(s_date_layer, date_buffer);
+
 }
 
 static void main_window_load(Window *window) {
@@ -61,6 +70,19 @@ static void main_window_load(Window *window) {
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  // Create date TextLayer
+  s_date_layer = text_layer_create(GRect(0, 12, 144, 30));
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Add to Window
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+
+  //
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_date_layer, s_date_font);
   
   // Create temperature Layer
   s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
@@ -99,6 +121,9 @@ static void main_window_unload(Window *window) {
   // Destroy weather elements
   text_layer_destroy(s_weather_layer);
   fonts_unload_custom_font(s_weather_font);
+  // 
+  fonts_unload_custom_font(s_date_font);
+  text_layer_destroy(s_date_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
