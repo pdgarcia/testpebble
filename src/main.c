06481400fc8c8072f116b307ff38b4b977e5173c
Pablo Garcia <pdgarcia@gmail.com>
2015-09-09 00:06:08 +0200

    Battery Layout added

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <pebble.h>
-  
+
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
-  
+
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_date_layer, *s_weather_layer;
 static int s_battery_level;
@@ -12,15 +12,17 @@ static GFont s_time_font, s_date_font, s_weather_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
+static Layer *s_battery_layer;
+
 static void update_time() {
   // Get a tm structure
-  time_t temp = time(NULL); 
+  time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
   static char buffer[] = "00:00";
   static char date_buffer[16];
-  
+
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     //Use 2h hour format
@@ -32,7 +34,7 @@ static void update_time() {
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
-  
+
   // Copy date into buffer from tm structure
   strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
   // Show the date
@@ -43,6 +45,23 @@ static void update_time() {
 static void battery_callback(BatteryChargeState state) {
   // Record the new battery level
   s_battery_level = state.charge_percent;
+  // Update meter
+  layer_mark_dirty(s_battery_layer);
+}
+
+static void battery_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+
+  // Find the width of the bar
+  int width = (int)(float)(((float)s_battery_level / 100.0F) * 114.0F);
+
+  // Draw the background
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+  // Draw the bar
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), 0, GCornerNone);
 }
 
 static void main_window_load(Window *window) {
@@ -51,7 +70,7 @@ static void main_window_load(Window *window) {
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  
+
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -61,7 +80,7 @@ static void main_window_load(Window *window) {
     text_layer_set_text_color(s_time_layer, GColorBlack);
   #endif
   text_layer_set_text(s_time_layer, "00:00");
-  
+
   //Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
@@ -71,7 +90,7 @@ static void main_window_load(Window *window) {
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  
+
   // Create date TextLayer
   s_date_layer = text_layer_create(GRect(0, 12, 144, 30));
   #ifdef PBL_COLOR
@@ -88,7 +107,7 @@ static void main_window_load(Window *window) {
   //
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
   text_layer_set_font(s_date_layer, s_date_font);
-  
+
   // Create temperature Layer
   s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
   text_layer_set_background_color(s_weather_layer, GColorClear);
@@ -99,40 +118,48 @@ static void main_window_load(Window *window) {
   #endif
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
   text_layer_set_text(s_weather_layer, "Loading...");
-  
+
   // Create second custom font, apply it and add to Window
   s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
   text_layer_set_font(s_weather_layer, s_weather_font);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
-  
+
   // Make sure the time is displayed from the start
   update_time();
+
+  // Create battery meter Layer
+  s_battery_layer = layer_create(GRect(14, 54, 115, 2));
+  layer_set_update_proc(s_battery_layer, battery_update_proc);
+
+  // Add to Window
+  layer_add_child(window_get_root_layer(window), s_battery_layer);
 }
 
 static void main_window_unload(Window *window) {
   //Unload GFont
   fonts_unload_custom_font(s_time_font);
-  
+
   //Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
 
   //Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
-  
+
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
-  
+
   // Destroy weather elements
   text_layer_destroy(s_weather_layer);
   fonts_unload_custom_font(s_weather_font);
   // Destroy Date
   fonts_unload_custom_font(s_date_font);
   text_layer_destroy(s_date_layer);
+  layer_destroy(s_battery_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
-  
+
   // Get weather update every 30 minutes
   if(tick_time->tm_min % 30 == 0) {
     // Begin dictionary
@@ -152,7 +179,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   static char temperature_buffer[8];
   static char conditions_buffer[32];
   static char weather_layer_buffer[32];
-  
+
   // Read first item
   Tuple *t = dict_read_first(iterator);
 
@@ -174,7 +201,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     // Look for next item
     t = dict_read_next(iterator);
   }
-  
+
   // Assemble full string and display
   snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
   text_layer_set_text(s_weather_layer, weather_layer_buffer);
@@ -191,7 +218,7 @@ static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResul
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
-  
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
@@ -204,19 +231,19 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-  
+
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
+
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
-  
+
   // Register for battery level updates
   battery_state_service_subscribe(battery_callback);
-  
+
   // Open AppMessage
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
